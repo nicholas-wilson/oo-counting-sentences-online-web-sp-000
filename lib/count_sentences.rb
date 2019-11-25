f38180a3dae4383bbf8742c6d836261a6c40ee6b
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-11-25 22:15:52 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,43 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      return true
+    else
+      return false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true
+    else
+      return false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true
+    else
+      return false
+    end
   end
 
   def count_sentences
+    words = self.split(" ")
+    sentences = []
+    sentence = ""
 
+    words.each do |word|
+      if (word.sentence? || word.question? || word.exclamation?)
+        sentence << word
+        sentences << sentence
+      else
+        sentence << word
+      end
+    end
+    sentences.length
   end
+
 end
